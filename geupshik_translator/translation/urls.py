5ee8a65e97891b9de71fea63e10e8f7cf6124c8b
Mark Kim <viking617617@gmail.com>
2017-11-10 22:01:37 +0900

    TranslationCreateForm, TranslationEditForm forms.py에 추가

--- geupshik_translator/translation/urls.py
+++ geupshik_translator/translation/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.translator_home, name='home')
+    url(r'^$', views.translation_create, name='home')
 ]
--- geupshik_translator/translation/views.py
+++ geupshik_translator/translation/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .forms import TranslationCreateForm
 
+def translation_create(request):
 
-def translator_home(request):
+    form = TranslationCreateForm()
     context = {
-
+        'form': form
     }
-    return render(request, 'translation/home.html', context)
+    return render(request, 'translation/form.html', context)
