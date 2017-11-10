ea6c2805d23605c7ea097fa35d385138a2c18aeb
John Sangwon Suh <johnsuh94@gmail.com>
2017-11-11 03:47:01 +0900

    html and add form

--- geupshik_translator/translation/forms.py
+++ geupshik_translator/translation/forms.py
@@ -3,7 +3,12 @@ from django import forms
 
 # 번역을 할 텍스트를 넣는 form
 class TranslationCreateForm(forms.Form):
-    pre_translated_text = forms.CharField(widget=forms.Textarea)
+    pre_translated_text = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                'class': 'translate-form',
+                'placeholder': '번역할 텍스트를 입력하세욧',
+            }))
 
 
 # 번역 수정을 제안할 텍스트를 넣는 form
